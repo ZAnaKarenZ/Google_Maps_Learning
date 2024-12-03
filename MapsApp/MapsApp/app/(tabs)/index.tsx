5ae360a72cf9908a2ebc7c54a19d1fe31dc80977
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-12-02 18:49:33 -0600

    traffic

--- MapsApp/MapsApp/app/(tabs)/index.tsx
+++ MapsApp/MapsApp/app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import MapView, { Marker } from 'react-native-maps';
 import { useState, useEffect } from 'react';
 import * as Location from 'expo-location';
+import { View, Button, StyleSheet } from 'react-native';
+
 
 const MostrarMapa = () => {
   const [location, setLocation] = useState(null);
-
+  const [showTraffic, setShowTraffic] = useState(false);
+  
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -19,26 +22,55 @@ const MostrarMapa = () => {
   }, []);
 
   return (
-    <MapView
-      style={{ flex: 1 }}
-      region={{
-        latitude: location?.coords.latitude || 37.78825,
-        longitude: location?.coords.longitude || -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    >
-      {location && (
-        <Marker
-          coordinate={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          }}
-          title="Mi ubicación"
-        />
-      )}
+    <View style={styles.container}>
+      <MapView
+        style={ styles.map }
+        region={{
+          latitude: location?.coords.latitude || 37.78825,
+          longitude: location?.coords.longitude || -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsTraffic ={showTraffic}
+      >
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Mi ubicación"
+          />
+        )}
     </MapView>
+      <View style={styles.buttonContainer}>
+        <Button
+          title={showTraffic ? 'Ocultar tráfico' : 'Mostrar tráfico'}
+          onPress={() => setShowTraffic(!showTraffic)}
+        />
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    height: "8%",
+    right: 20,
+    top: 100,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+    borderRadius: 10,
+    padding: 10,
+  },
+});
 
 export default MostrarMapa;
